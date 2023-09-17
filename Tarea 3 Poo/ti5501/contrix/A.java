5c01d779175fe74fb5d9839324acd5a2c4e34b2a
AndreAG26 <103344222+AndreAG26@users.noreply.github.com>
2023-09-17 13:30:59 -0600

    kjk

---
+++ Tarea 3 Poo/ti5501/contrix/A.java
@@ -0,0 +1,28 @@
+package ti5501.contrix;
+
+
+public class A
+{
+  // instance variables - replace the example below with your own
+  protected String a2;
+
+  /**
+   * Constructor for objects of class A
+   */
+  public A()
+  {
+    // initialise instance variables
+    
+  }
+
+  /**
+   * An example of a method - replace this comment with your own
+   * 
+   * @param  y   a sample parameter for a method
+   * @return     the sum of x and y 
+   */
+  public void sampleMethod(int y)
+  {
+    
+  }
+}
---
+++ Tarea 3 Poo/ti5501/contrix/D.java
@@ -0,0 +1,29 @@
+package ti5501.contrix;
+
+public class D
+{
+  // instance variables - replace the example below with your own
+  private String d1;
+
+  // Cosntructor
+  public D()
+  {
+    // initialise instance variables
+    
+  }
+
+  
+  public void dM1(){
+    
+  }
+  
+  public int dM2(){
+    int c=0;
+    return c;
+  }
+  
+  public boolean dM3(){
+    boolean c=true;
+    return c;
+  }
+}
